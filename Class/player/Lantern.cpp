44b35f2044070966db92b031a22c5c0d04c85dc8
pepe <kousaku5963@gmail.com>
2023-11-15 23:51:48 +0900

    ランタンのbehaviorを作成。通常状態はランタンのintensityの値をを少しだけ上げ下げさせ、点滅させている。 点滅状態はランタンが連続して、大きく点滅する。点滅する回数は1~3のランダムで決まる

--- Class/player/Lantern.cpp
+++ Class/player/Lantern.cpp
@@ -1,4 +1,5 @@
 #include "Lantern.h"
+#include <random>
 
 void Lantern::Init() {
 	handleModel_ = LWP::Resource::LoadModel("Lantern/Lantern_Handle.obj");
@@ -12,13 +13,18 @@ void Lantern::Init() {
 	light_->decay = 1.5f;
 	light_->isActive = true;
 	light_->name = "Lantern";
-	
+
 	// 親子関係をセット
 	model_->transform.Parent(&handleModel_->transform);
 	light_->transform.Parent(&model_->transform);
+
+	LightBehaviorNormalInit();
 }
 
 void Lantern::Update() {
+	// ライトのふるまい
+	LightBehavior();
+
 #if _DEBUG
 	ImGui::Begin("Lantern");
 	if (ImGui::TreeNode("Handle")) {
@@ -39,4 +45,122 @@ void Lantern::Update() {
 
 LWP::Object::WorldTransform* Lantern::Grab() {
 	return &handleModel_->transform;
+}
+
+void Lantern::LightBehavior() {
+	// 初期化
+	if (behaviorRequest_) {
+		//  振るまいを変更
+		behavior_ = behaviorRequest_.value();
+		switch (behavior_) {
+		case Behavior::kNormal:
+		default:
+			LightBehaviorNormalInit();
+			break;
+			// 攻撃行動
+		case Behavior::kFlicker:
+			LightBehaviorFlickerInit();
+			break;
+		}
+		// 振るまいリクエストをリセット
+		behaviorRequest_ = std::nullopt;
+	}
+
+	// 更新処理
+	switch (behavior_) {
+	case Behavior::kNormal:
+	default:
+		LightBehaviorNormalUpdate();
+		break;
+		// 攻撃行動
+	case Behavior::kFlicker:
+		LightBehaviorFlickerUpdate();
+		break;
+	}
+}
+
+void Lantern::LightBehaviorNormalInit() {
+	// 光の強さを戻す
+	light_->intensity = 1.0f;
+	// 振幅
+	lightAmplitude_ = 0;
+	// 周期
+	intensityCycle_ = 0;
+	// 1往復何フレーム
+	intensityCycleFrame_ = 60;
+
+	// 乱数生成器の初期化
+	std::random_device seedGenerator;
+	std::mt19937 randomEngine(seedGenerator());
+
+	// 現在のフレーム
+	lightNormal_.currentFrame = 0;
+	// 終了フレームを乱数で生成
+	std::uniform_real_distribution<float> distribution(600.0f, 900.0f);
+	lightNormal_.EndFrame = static_cast<int>(distribution(randomEngine));
+}
+
+void Lantern::LightBehaviorNormalUpdate() {
+	if (lightNormal_.currentFrame >= lightNormal_.EndFrame) {
+		// 点滅に移行
+		behaviorRequest_ = Behavior::kFlicker;
+	}
+
+	// 振幅の最大値&最小値
+	const float kMaxAmplitude = 0.2f;
+	// 周期(2秒で1往復)
+	const float kCycleSpeed = M_PI / intensityCycleFrame_;
+	intensityCycle_ += kCycleSpeed;
+
+	//振幅を計算
+	lightAmplitude_ = kMaxAmplitude * sinf(intensityCycle_);
+	// lightに計算結果を反映
+	light_->intensity = 1.0f - lightAmplitude_;
+
+	lightNormal_.currentFrame++;
+}
+
+void Lantern::LightBehaviorFlickerInit() {
+	// 光の強さを戻す
+	light_->intensity = 1.0f;
+	lightAmplitude_ = 0;
+	intensityCycle_ = 0;
+	// 1往復何フレーム
+	intensityCycleFrame_ = 8;
+
+	// 乱数生成器の初期化
+	std::random_device seedGenerator;
+	std::mt19937 randomEngine(seedGenerator());
+
+	// 終了フレームを乱数で生成
+	std::uniform_int_distribution<> distribution(1,3);
+	flickerCount_ = distribution(randomEngine);
+
+	// 現在のフレーム
+	lightFlicker_.currentFrame = 0;
+	// 何回点滅させるかを設定
+	lightFlicker_.EndFrame = intensityCycleFrame_ / 2 * flickerCount_;
+}
+
+void Lantern::LightBehaviorFlickerUpdate() {
+	if (lightFlicker_.currentFrame >= lightFlicker_.EndFrame) {
+		// 通常状態に移行
+		behaviorRequest_ = Behavior::kNormal;
+	}
+
+	// 振幅の最大値&最小値
+	const float kMaxAmplitude = 0.5f;
+	// 8フレームで1往復
+	const float kCycleSpeed = M_PI / (intensityCycleFrame_ / 2);
+	intensityCycle_ += kCycleSpeed;
+
+	//振幅を計算
+	lightAmplitude_ = kMaxAmplitude * sinf(intensityCycle_);
+	// lightに計算結果を反映
+	light_->intensity = 1.0f - fabs(lightAmplitude_);
+
+	lightFlicker_.currentFrame++;
+	ImGui::Begin(" ");
+	ImGui::Text("FlickerCount:%d", flickerCount_);
+	ImGui::End();
 }
--- Class/player/Lantern.h
+++ Class/player/Lantern.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Adapter.h>
+#include <optional>
 
 /// <summary>
 /// ランタン
@@ -25,6 +26,17 @@ public: // ** メンバ関数 ** //
 	/// <param name="direction">投げる方向のベクトル（正規化済みを送ること）</param>
 	void Throw(LWP::Math::Vector2 direction);
 
+	// ライトの振るまい
+	void LightBehavior();
+
+	// 通常時の初期化
+	void LightBehaviorNormalInit();
+	// 通常時の更新
+	void LightBehaviorNormalUpdate();
+	// 光の点滅初期化
+	void LightBehaviorFlickerInit();
+	// 光の点滅更新
+	void LightBehaviorFlickerUpdate();
 
 private: // ** メンバ変数 ** //
 
@@ -36,4 +48,34 @@ private: // ** メンバ変数 ** //
 
 	// 掴まれているフラグ
 	bool isGrabed;
+
+	// 振るまい
+	enum class Behavior {
+		kNormal,	 // 通常状態
+		kFlicker // 光がチカチカ状態
+	};
+	Behavior behavior_ = Behavior::kNormal;
+	// 次の振るまいリクエスト
+	std::optional<Behavior> behaviorRequest_ = std::nullopt;
+
+	// 各ふるまいで扱う時間
+	struct BehaviorFrame {
+		int currentFrame = 0;  // 現在のフレーム
+		int EndFrame;		   // 終了フレーム
+	};
+	// 通常状態のライト
+	BehaviorFrame lightNormal_;
+	// 点滅時のライト
+	BehaviorFrame lightFlicker_;
+
+	// ライトの点滅の振幅
+	float lightAmplitude_;
+	// ライトの点滅の周期
+	float intensityCycle_;
+	// 1往復を何フレームで行うか
+	int intensityCycleFrame_;
+
+	// ライトの点滅回数(通常状態では使用しない)
+	int flickerCount_;
+
 };
