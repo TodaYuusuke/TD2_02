a139d74ae8b9ebcd984a376fc99752d47c5f7e90
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2023-11-30 05:13:01 +0900

    とりあえずのタイトルマージ完了

---
+++ Class/effect/Fire.cpp
@@ -0,0 +1,108 @@
+#include "Fire.h"
+
+using namespace LWP;
+using namespace LWP::Input;
+using namespace LWP::Primitive;
+using namespace LWP::Math;
+using namespace LWP::Utility;
+
+void Fire::Initialize(LWP::Math::Vector3 Position)
+{
+	// テクスチャ読み込み
+	Texture = LWP::Resource::LoadTexture("Fire.png");
+
+	//乱数生成器の初期化
+	std::mt19937 randomEngine(seedGenerator());
+
+	light_ = LWP::Object::CreateInstance<LWP::Object::PointLight>();
+	light_->transform.translation = { -0.1f,4.0f,-0.2f };
+	light_->color = LWP::Utility::Color::KelvinToRGB(1700);
+	light_->radius = 1.9f;
+	light_->decay = 1.5f;
+	light_->isActive = true;
+	light_->name = "Fire";
+	
+	position_ = Position;
+	position_.y += 0.01;
+}
+
+void Fire::Update() {
+
+	// 新しいパーティクルを追加
+	if (particles.size() < kMaxParticles)
+	{
+		particles.push_back(CreateParticle(randomEngine, position_));
+	}
+
+	// 上へあげる処理
+	for (std::vector<Particle>::iterator it = particles.begin(); it != particles.end();)
+	{
+		if (it->fire->isActive == true)
+		{
+			const float kDeltaTime = 0.1f / 60.0f;
+			it->fire->transform.translation.y += it->velocity * kDeltaTime;
+			it->fire->transform.scale.x -= 0.0005f;
+			it->fire->transform.scale.y -= 0.0005f;
+			it->fire->transform.scale.z -= 0.0005f;
+
+
+			// スケールが0なったら消す
+			if (it->fire->transform.scale.x <= 0.0f)
+			{
+				it->fire->isActive = false;
+				ResetParticle(it, randomEngine, position_);
+			}
+			else
+			{
+				++it;
+			}
+		}
+	}
+}
+
+
+void Fire::Draw()
+{
+}
+
+
+Particle Fire::CreateParticle(std::mt19937& randomEngine, LWP::Math::Vector3 position)
+{
+	Particle particle;
+	particle.velocity = generateRandomValue(randomEngine, 0.1f, 2.0f);
+	particle.bithPosition.x = generateRandomValue(randomEngine, position.x - 0.04f, position.x + 0.04f);
+	particle.bithPosition.y = position.y;
+	particle.bithPosition.z = position.z;
+
+	
+	particle.fire = LWP::Primitive::CreateInstance<Mesh>();
+	particle.fire = LWP::Resource::LoadModel("Bonfire/fire.obj");
+	particle.fire->transform.translation = { particle.bithPosition.x,particle.bithPosition.y,particle.bithPosition.z };
+	particle.fire->transform.scale = { 0.03f, 0.03f, 0.02f };
+	particle.fire->texture = Texture;
+	particle.fire->material.enableLighting = false;
+	particle.fire->isActive = true;
+
+
+	return particle;
+}
+
+
+// 乱数生成器を受け取る関数
+float Fire::generateRandomValue(std::mt19937& generation, float min, float max)
+{
+	std::uniform_real_distribution<float> dis(min, max);
+	return dis(generation);
+}
+
+void Fire::ResetParticle(std::vector<Particle>::iterator it, std::mt19937& randomEngine, LWP::Math::Vector3 position)
+{
+	float velocity = generateRandomValue(randomEngine, 0.1f, 2.0f);
+	float lifeTime = generateRandomValue(randomEngine, 1.0f, 3.0f);
+	float bithPosition = generateRandomValue(randomEngine, position.x - 0.04f, position.x + 0.04f);
+
+	it->fire->transform.translation = { bithPosition,position_.y,position_.z };
+	it->fire->transform.scale = { 0.03f, 0.03f, 0.02f };
+	it->velocity = velocity;
+	it->fire->isActive = true;
+}
--- Class/sence/Title.h
+++ Class/sence/Title.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <Adapter.h>
 #include "../Engine/scene/IScene.h"
+#include "../sence/StageSelectScene.h"
+#include"../stage/MapChip/Derved.h"
+#include"../effect/Fire.h"
 
 class Title final
 	: public IScene {
@@ -28,9 +31,17 @@ private: //*** これより先に必要な処理や変数を記述 ***//
 	// BGM
 	LWP::Resource::Audio* bgm_[BGM_NUM];
 
-	LWP::Primitive::Sphere* sphere;
-	// プレイヤーモデル
-	LWP::Object::WorldTransform player_;
+	// 背景
+	LWP::Primitive::Surface* backGround_;
+
+	//タイトル
+	LWP::Primitive::Surface* logo_omote_;
+	LWP::Primitive::Surface* logo_ura;
+
+
+	// モデル
+	LWP::Primitive::Mesh* model_ = nullptr;
+
 	LWP::Primitive::Mesh* headModel_ = nullptr;				//頭
 	LWP::Primitive::Mesh* bodyModel_ = nullptr;				//上半身
 	LWP::Primitive::Mesh* leftForeArmModel_ = nullptr;		//左腕　手から肘
@@ -38,4 +49,18 @@ private: //*** これより先に必要な処理や変数を記述 ***//
 	LWP::Primitive::Mesh* rightForeArmModel_ = nullptr;		//右腕　手から肘
 	LWP::Primitive::Mesh* rightUpperArmModel_ = nullptr;	//右腕　肘から肩
 	LWP::Primitive::Mesh* footModel_ = nullptr;				//下半身
+
+	LWP::Primitive::Mesh* bonfire_ = nullptr;
+
+	LWP::Object::PointLight* light_;
+
+	// プレイヤーのスタート地点
+	LWP::Math::Vector3 startPosition_;
+	// カメラのオフセット
+	LWP::Math::Vector3 cameraOffset_;
+
+	//床
+	Floor floor;
+	OutLineWall wall[5];
+	Fire fire;
 };
